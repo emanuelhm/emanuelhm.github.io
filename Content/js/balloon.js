cb77365a49702a8f1f10cd359736c6fa9aa9271b
emanuelhm <miyagawa.emanuel@gmail.com>
2018-04-05 18:01:26 -0300

    Populando grupo e adicionando CriarGrupo

--- Content/js/balloon.js
+++ Content/js/balloon.js
@@ -1,11 +1,22 @@
-﻿function simularCriacaoUsuario() {
+﻿$("#RegisterForm").submit(function (e) {
+    e.preventDefault();
     sessionStorage.setItem("cor", $("#Cor").val());
     window.location.href = "/inicio.html";
-}
+});
 
-function simularCriacaoGrupo() {
+$("#modalCriarGrupo").submit(function (e) {
+    e.preventDefault();
     sessionStorage.setItem("nomeGrupo", $("#Nome").val());
     sessionStorage.setItem("descricaoGrupo", $("#Descricao").val());
     sessionStorage.setItem("corGrupo", $("#Cor").val());
     window.location.href = "/grupo.html";
-}
+});
+
+$(document).ready(function () {
+    $(".mudarCor").each(function (index, value) {
+        $(value).css("background-color", "#" + sessionStorage.getItem("cor"));
+    });
+    $(".mudarCorFonte").each(function (index, value) {
+        $(value).css("color", "#" + sessionStorage.getItem("cor"));
+    });
+});
