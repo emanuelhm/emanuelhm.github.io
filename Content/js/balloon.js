0f67cd1ba1eebaef38e6bd6d773769a6bb103777
emanuelhm <miyagawa.emanuel@gmail.com>
2018-04-05 17:34:59 -0300

    Usando sessionStorage

--- Content/js/balloon.js
+++ Content/js/balloon.js
@@ -1,11 +1,11 @@
 ﻿function simularCriacaoUsuario() {
-    var cor = $("#Cor").val();
+    sessionStorage.setItem("cor", $("#Cor").val());
     window.location.href = "/inicio.html";
 }
 
 function simularCriacaoGrupo() {
-    var nomeGrupo = $("#Nome").val();
-    var descricaoGrupo = $("#Descricao").val();
-    var corGrupo = $("#Cor").val();
+    sessionStorage.setItem("nomeGrupo", $("#Nome").val());
+    sessionStorage.setItem("descricaoGrupo", $("#Descricao").val());
+    sessionStorage.setItem("corGrupo", $("#Cor").val());
     window.location.href = "/grupo.html";
 }
